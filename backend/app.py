14e282cd66f542591a98d7824b99ced98396bd5e
Bada-Don <harshitashwani@gmail.com>
2025-07-02 11:31:32 +0530

    CORS blocked API call

--- backend/app.py
+++ backend/app.py
@@ -14,7 +14,7 @@ import csv
 import os
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["https://boliyan.vercel.app"])
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
